46dfb5adc3fe3a26c6532d00d9535ce573a2a315
Zeh237 <zehbrien@gmail.com>
2025-05-20 06:29:54 +0100

    created projects module

--- core/urls.py
+++ core/urls.py
@@ -22,6 +22,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('users.urls', namespace='accounts')),
+    path('projects/', include('project.urls', namespace='project'))
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
